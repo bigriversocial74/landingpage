8fdeac3abd9074403e04d17fba2ff1683cfb1f39
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-28 23:13:47 -0700

    Apply updater version override in license manager v65

--- portal/vp3-license-manager.php
+++ portal/vp3-license-manager.php
@@ -3,4 +3,5 @@ declare(strict_types=1);
 
 require __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/vp3-license-settings-store.php';
+require_once __DIR__ . '/vp3-update-version-override.php';
 require __DIR__ . '/vp3-license.php';
