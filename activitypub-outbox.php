bbe5fb8d749d1521d16626efa84f13ee8e50d7a6
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-30 12:09:10 -0700

    Add ActivityPub outbox endpoint v66F

---
+++ activitypub-outbox.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+define('NMM_PUBLIC_PAGE', true);
+require __DIR__ . '/portal/bootstrap.php';
+require_once __DIR__ . '/portal/activitypub-service.php';
+
+try {
+    if (!activitypub_settings()['enabled']) throw new RuntimeException('Federation disabled.');
+    activitypub_json_response(activitypub_outbox_document());
+} catch (Throwable) {
+    http_response_code(404);
+    exit;
+}
